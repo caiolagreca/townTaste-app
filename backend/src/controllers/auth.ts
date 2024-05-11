f3c9d0f2230b27389ddf8518b0f54952349a42b3
Caio Lagreca <caiomiranda701@hotmail.com>
2024-05-11 23:22:34 +1000

    fixing TS bug

--- backend/src/controllers/auth.ts
+++ backend/src/controllers/auth.ts
@@ -8,45 +8,43 @@ import { ErrorCode } from "../exceptions/root";
 import { LoginSchema, SignUpSchema } from "../schema/users";
 import { NotFoundException } from "../exceptions/not-found";
 
-export const signup: RequestHandler = async (
-  req: Request,
-  res: Response,
-  next: NextFunction
-) => {
-  SignUpSchema.parse(req.body);
-  const {
-    email,
-    password,
-    firstName,
-    lastName,
-    age,
-    phoneNumber,
-    address,
-    profilePhoto,
-  } = req.body;
-
-  let user = await prismaClient.user.findFirst({ where: { email } });
-  if (user) {
-    return next(
-      new BadRequestsException(
-        "User already exists.",
-        ErrorCode.USER_ALREADY_EXISTS
-      )
-    );
-  }
-  user = await prismaClient.user.create({
-    data: {
+export const signup: RequestHandler = async (req, res, next) => {
+  try {
+    SignUpSchema.parse(req.body);
+    const {
+      email,
+      password,
       firstName,
       lastName,
-      email,
       age,
       phoneNumber,
       address,
-      password: hashSync(password, 10),
       profilePhoto,
-    },
-  });
-  res.json({ success: true, user });
+    } = req.body;
+
+    let user = await prismaClient.user.findUnique({ where: { email } });
+    if (user) {
+      throw new BadRequestsException(
+        "User already exists.",
+        ErrorCode.USER_ALREADY_EXISTS
+      );
+    }
+    user = await prismaClient.user.create({
+      data: {
+        firstName,
+        lastName,
+        email,
+        age,
+        phoneNumber,
+        address,
+        password: hashSync(password, 10),
+        profilePhoto,
+      },
+    });
+    res.json({ success: true, user });
+  } catch (error) {
+    next(error);
+  }
 };
 
 export const login: RequestHandler = async (
