6a0331e8e439e31bfe261c8295f55ffe067b94dd
Caio Lagreca <caiomiranda701@hotmail.com>
2024-05-22 18:56:20 +1000

    Remove server folder

--- server/src/routes/auth.ts
+++
@@ -1,8 +0,0 @@
-import { Router } from "express";
-import { signup } from "../controllers/auth";
-
-const authRoutes: Router = Router();
-
-authRoutes.post("/signup", signup);
-
-export default authRoutes;
